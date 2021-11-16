9d56e6d30a0deb16d2a3d33143893c839f2f5e0b
Ulises <qtulises@gmail.com>
2021-11-16 13:48:17 -0600

    Adding the modal window succesfully

--- alert.js
+++ alert.js
@@ -1,4 +1,32 @@
-function run_alert(){
-    document.getElementById("btn-grad").style.display = "none";
-    document.getElementById("donaciones").style.display = "block";
-}   
+if(document.getElementById("btn-grad")){
+    var modal = document.getElementById("tvesModal");
+    var btn = document.getElementById("btn-grad");
+    var span = document.getElementsByClassName("close")[0];
+    var body = document.getElementsByTagName("body")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
